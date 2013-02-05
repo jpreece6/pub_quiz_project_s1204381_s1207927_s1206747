6965c0b9e1531210809de2a593e0a1e770cb533b
Joshua Preece <looterwar@gmail.com>
2013-02-05 23:30:36 +0000

    Test Commit

--- Pub Quiz/src/server/Server.java
+++ Pub Quiz/src/server/Server.java
@@ -6,8 +6,11 @@ public class Server {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new Server();
+	}
+	
+	public Server() {
+		
 	}
 
 }
