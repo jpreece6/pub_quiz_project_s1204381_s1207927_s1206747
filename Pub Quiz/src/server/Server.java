d7b1fe8a135ffc7b55b44c0906c269f2c8957bdd
Joshua Preece <looterwar@gmail.com>
2013-02-06 00:01:10 +0000

    Init Commit

--- Pub Quiz/src/server/Server.java
+++ Pub Quiz/src/server/Server.java
@@ -1,17 +1,35 @@
 package server;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Server {
 
-	/**
-	 * @param args
-	 */
+	private static final int NUM_CLIENTS = 20;
+	private static final ExecutorService exe = Executors.newFixedThreadPool(NUM_CLIENTS);
+
+	private Socket clientSocket;
+	private static ServerSocket serverSocket;
+	
 	public static void main(String[] args) {
 		new Server();
 	}
 	
 	public Server() {
-		// something
-		// more
+		
+		for(;;) {
+			
+			try {
+				
+				clientSocket = serverSocket.accept();
+				
+			} catch (Exception ex) {
+				
+			}
+			
+		}
 	}
 
 }
